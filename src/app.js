41df433aaf1bb8ab7b3800707c7a391b7f361613
Felipe Lopes <felipemlopes0009@gmail.com>
2024-03-27 11:10:39 -0400

    GET and POST response

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-console.log(uuidv4());
+app.get("/", (req, res) => {
+  res.send("Change the world!");
+});
 
+app.post('/', (req, res) => {
+  res.send('This is a post request!');
+})
 
+app.listen(PORT, () => {
+  console.log("App listening on port " + PORT + "!");
+});
