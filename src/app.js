e60dff96355441af01bd31bf4cb6010a9514366c
Felipe Lopes <felipemlopes0009@gmail.com>
2024-03-30 18:10:31 -0400

    Await mongoose to connect before the server starts

--- src/app.js
+++ src/app.js
@@ -1,17 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
-console.log(uuidv4());
-console
+const books = [
+  { name: "Atomic Habits", industry: "self-help" },
+  { name: "Surviving a Startup", industry: "entrepreneurship" },
+  { name: "Harry Potter", industry: "fantasy" },
+];
+
+const home = "Home Page";
+
 app.get("/", (req, res) => {
-  res.send("Change the world!");
+  res.send({ home });
 });
 
-app.post('/', (req, res) => {
-  res.send('This is a post request!!');
-})
-
-app.listen(PORT, () => {
-  console.log("App listening on port " + PORT);
+app.get("/api/books", (req, res) => {
+  res.send({ data: books });
 });
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post("/", (req, res) => {
+  res.send("This is a post request!!");
+});
+
+const start = async() => {
+  await mongoose.connect();
+  
+  app.listen(PORT, () => {
+    console.log("App listening on port" + PORT);
+  })
+};
+
+start();
